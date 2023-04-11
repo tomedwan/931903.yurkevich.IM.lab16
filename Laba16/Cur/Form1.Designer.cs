0060234d548e7fba7c97b5aef09eee6ef9f27bc0
tomedwan <ilyauiv2000@gmail.com>
2023-04-11 21:09:39 +0700

    1

---
+++ Laba16/Cur/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Cur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Nextbt = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Sale = new System.Windows.Forms.Button();
+            this.Buy = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edRate = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.Nextbt);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.Sale);
+            this.panel1.Controls.Add(this.Buy);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.edRate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1382, 101);
+            this.panel1.TabIndex = 0;
+            // 
+            // Nextbt
+            // 
+            this.Nextbt.Location = new System.Drawing.Point(1232, 58);
+            this.Nextbt.Name = "Nextbt";
+            this.Nextbt.Size = new System.Drawing.Size(150, 37);
+            this.Nextbt.TabIndex = 12;
+            this.Nextbt.Text = "Расчитать дальше";
+            this.Nextbt.UseVisualStyleBackColor = true;
+            this.Nextbt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(656, 16);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(569, 77);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Sale
+            // 
+            this.Sale.Location = new System.Drawing.Point(531, 52);
+            this.Sale.Margin = new System.Windows.Forms.Padding(4);
+            this.Sale.Name = "Sale";
+            this.Sale.Size = new System.Drawing.Size(117, 23);
+            this.Sale.TabIndex = 9;
+            this.Sale.Text = "Продать";
+            this.Sale.UseVisualStyleBackColor = true;
+            this.Sale.Click += new System.EventHandler(this.Sale_Click);
+            // 
+            // Buy
+            // 
+            this.Buy.Location = new System.Drawing.Point(531, 16);
+            this.Buy.Margin = new System.Windows.Forms.Padding(4);
+            this.Buy.Name = "Buy";
+            this.Buy.Size = new System.Drawing.Size(117, 24);
+            this.Buy.TabIndex = 8;
+            this.Buy.Text = "Купить";
+            this.Buy.UseVisualStyleBackColor = true;
+            this.Buy.Click += new System.EventHandler(this.Buy_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(375, 54);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(139, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(295, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Доллары";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(375, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(139, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(1232, 22);
+            this.btStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(150, 29);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Начать";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Рубли";
+            // 
+            // edRate
+            // 
+            this.edRate.DecimalPlaces = 2;
+            this.edRate.Location = new System.Drawing.Point(170, 22);
+            this.edRate.Margin = new System.Windows.Forms.Padding(4);
+            this.edRate.Name = "edRate";
+            this.edRate.Size = new System.Drawing.Size(128, 22);
+            this.edRate.TabIndex = 1;
+            this.edRate.Value = new decimal(new int[] {
+            7433,
+            0,
+            0,
+            131072});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текущий курс";
+            // 
+            // chart1
+            // 
+            chartArea3.AxisX.Minimum = 0D;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 101);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.IsValueShownAsLabel = true;
+            series3.LabelFormat = "F2";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1382, 604);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-3, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(160, 71);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 22);
+            this.textBox4.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 705);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Sale;
+        private System.Windows.Forms.Button Buy;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown edRate;
+        private System.Windows.Forms.Button Nextbt;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Laba16/Cur/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double k = 0.05;
+        int i = -1;
+        double rate;
+        double rubles = 100;
+        int dollars = 0;
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (i < 0)
+            {
+                Random random = new Random();
+                rate = (double)edRate.Value;
+                chart1.Series[0].Points.Clear();
+                chart1.Series[0].Points.AddXY(0, rate);
+
+                i++;
+
+                textBox4.Text = rate.ToString("F2");
+                rate = rate * (1 + k * (random.NextDouble() - 0.5));
+                chart1.Series[0].Points.AddXY(i, rate);
+
+                textBox1.Text = rubles.ToString("F2");
+                textBox2.Text = dollars.ToString();
+                textBox3.Text = "          ";
+                edRate.Value = (decimal)rate;
+
+                btStart.Text = "Закончить";
+                label1.Text = "Примерный завтрашний курс";
+                label4.Text = "Сегодняшний курс";
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void Buy_Click(object sender, EventArgs e)
+        {
+            if (rubles >= rate) 
+            {
+                dollars++;
+                rubles -= rate;
+                textBox1.Text = rubles.ToString("F2");
+                textBox2.Text = dollars.ToString();
+                textBox3.Text = "Поздравляем с покупкой в нашем лохотроне";
+            }
+            else
+            {
+                textBox3.Text = "Бомж!"; 
+            }
+        }
+
+        private void Sale_Click(object sender, EventArgs e)
+        {
+            if (dollars >= 1)
+            {
+                dollars--;
+                rubles += rate;
+                textBox1.Text = rubles.ToString("F2");
+                textBox2.Text = dollars.ToString();
+                textBox3.Text = "Надеемся это даст вам надежду";
+            }
+            else
+            {
+                textBox3.Text = "В кормашке то пусто!"; 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i >= 0)
+            {
+                Random random = new Random();
+                rate = (double)edRate.Value;
+                chart1.Series[0].Points.Clear();
+                chart1.Series[0].Points.AddXY(0, rate);
+
+                i++;
+                textBox4.Text = rate.ToString("F2");
+                rate = rate * (1 + k * (random.NextDouble() - 0.5));
+
+                chart1.Series[0].Points.AddXY(i, rate);
+
+                textBox1.Text = rubles.ToString("F2");
+                textBox2.Text = dollars.ToString();
+                textBox3.Text = "          ";
+                edRate.Value = (decimal)rate;
+            }
+
+        }
+    }
+}
